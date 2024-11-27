c475e5496281d8966c17bf7fcb8a81d014751a8b
MartinKeithBuchreitz <Martinbu29@gmail.com>
2024-11-27 13:21:08 +0100

    load

--- src/main/scala/Accelerator.scala
+++ src/main/scala/Accelerator.scala
@@ -19,6 +19,7 @@ class Accelerator extends Module {
   val registers = Vec(60, RegInit(0.U(16.W)))
   var x = 0.U
   var y = 0.U
+  var index = 0.U
   var blackNeighbours = false.B
 
   switch(stateReg) {
@@ -85,24 +86,19 @@ class Accelerator extends Module {
         stateReg := done
       }
         .otherwise{
+          index = 0.U
           stateReg := load
         }
     }
     is(load) {
-      var i = y * 20.U
-      when(i < y * 20.U + 20.U) {
-        val reg1 = registers(i + 20.U)
-        registers(i) == reg1
+      when (index < 20.U){
+        registers(index) == registers(index+20.U)
+        registers(index+20.U) == registers(index+40.U)
+        io.address := (y+1.U)*20.U + index
+        registers(index+40.U) == io.dataRead
+      } .otherwise{
+        stateReg := pixelCheck
       }
-        .elsewhen(i < y * 20.U + 40.U) {
-          val reg2 = registers(i + 2*20.U) // finished here <3
-          registers(i) == reg2
-        }
-        .elsewhen(i < y * 20.U + 60.U){
-          io.address := i + 20.U
-          val reg3 = io.dataRead
-          registers(i) == reg3
-        }
     }
     is(done) {
       io.done := true.B
